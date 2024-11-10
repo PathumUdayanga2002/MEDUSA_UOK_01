65d85b3772b36a4048ff93e1e96476558f125ad3
Sandu70one <sanduaruna70one@gmail.com>
2024-11-10 23:47:54 +0530

    price pool component added
    couldn't added background color for each prices

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "../src/components/Navbar/Navbar";
 import Partners from "../src/components/Partners/Partners";
 import Home from "./components/Home/Home";
 import Timeline from "./components/Timeline/Timeline";
+import Pricepool from "./components/Pricepool/Pricepool";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
     <Navbar/> 
     <Home/>
     <Timeline/>
+    <Pricepool/>
     <Partners/>
     </>
   )
